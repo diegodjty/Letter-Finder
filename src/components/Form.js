e6d78eafb947246ea2371fbeb5bb931a04ce8183
Diego Taveras <diegotaveras2@outlook.com>
2020-04-06 22:08:08 -0400

    Add a form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Form = () => {
+
+    const [search, setSearch] = useState({
+        artist: '',
+        song: ''
+    })
+
+    const [error,setError] = useState(false);
+
+    const {artist,song} = search;
+    const updateState = e =>{
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const lookInformation = e =>{
+        e.preventDefault();
+
+        if(artist.trim() === '' || song.trim() === ''){
+            setError(true);
+            return;
+        }
+        setError(false);
+
+    }
+
     return (
         <div className="bg-info">
             <div className="container">
                 <div className="row">
                     <form
                       className="col card text-white bg-transparent mb-5 pt-5 pb-2"  
+                      onSubmit={lookInformation}
                     >
                         <fieldset>
                             <legend className="text-center">Songs letter finder</legend>
@@ -19,6 +47,8 @@ const Form = () => {
                                             className="form-control"
                                             name="artist"
                                             placeholder="Name of the artist"
+                                            onChange={updateState}
+                                            value={artist}
                                         />
                                     </div>
                                 </div>
@@ -30,10 +60,18 @@ const Form = () => {
                                             className="form-control"
                                             name="song"
                                             placeholder="Name of the song"
+                                            onChange={updateState}
+                                            value={song}
                                         />
                                     </div>
                                 </div>
                             </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary float-right"
+                            >
+                                Search
+                            </button>
                         </fieldset>
                     </form>
                 </div>
